a25ce9639e508e8b03a3ec75c8a37e4a65a666a7
Brent Salisbury <bsalisbu@redhat.com>
2023-08-30 20:35:28 -0400

    Initial windows support
    Signed-off-by: Brent Salisbury <bsalisbu@redhat.com>

---
+++ windows/nexd-windows-gui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace nexodus_windows_agent_ui
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
